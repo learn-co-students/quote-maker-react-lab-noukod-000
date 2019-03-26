6c8c6dc30d08dd88e006658454cc6e3382b1928c
GilTorch <infotorch2014@gmail.com>
2019-03-26 11:24:55 -0400

    Done.

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -7,17 +7,35 @@ class QuoteForm extends Component {
 
   state = {
     //set up a controlled form with internal state
+    author:"",
+    content:""
   }
 
   handleOnChange = event => {
     // Handle Updating Component State
+    this.setState({
+      [event.target.name]:event.target.value
+    })
   }
 
   handleOnSubmit = event => {
     // Handle Form Submit event default
+    event.preventDefault();
     // Create quote object from state
+    const { content, author }=this.state;
+    const quote={
+      id:uuid(),
+      author,
+      content,
+      votes:0
+    }
     // Pass quote object to action creator
+    this.props.dispatch(addQuote(quote))
     // Update component state to return to default state
+    this.setState({
+      author:"",
+      content:""
+    })
   }
 
   render() {
@@ -27,13 +45,15 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form onSubmit={ this.handleOnSubmit } className="form-horizontal">
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea
+                        name="content"
+                        onChange={ this.handleOnChange}
                         className="form-control"
-                        value={this.state.content}
+                        value={ this.state.content}
                       />
                     </div>
                   </div>
@@ -41,7 +61,9 @@ class QuoteForm extends Component {
                     <label htmlFor="author" className="col-md-4 control-label">Author</label>
                     <div className="col-md-5">
                       <input
+                        name="author"
                         className="form-control"
+                        onChange={this.handleOnChange}
                         type="text"
                         value={this.state.author}
                       />
