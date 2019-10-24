279df03efe9b4c6ff84bb7b93597abe20ada156d
Herna Michel <labellehernamichel@yahoo.fr>
2019-10-24 14:03:26 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,12 +1,15 @@
-export default function quotes(state = [], action) {
+export default (state = [], action) => {
   let index;
   let quote;
 
   switch (action.type) {
-    case "ADD_QUOTE":
-      return [].concat(state, action.quote)
-    case "REMOVE_QUOTE":
+
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
+
+    case 'REMOVE_QUOTE':
       return state.filter(quote => quote.id !== action.quoteId);
+
     case 'UPVOTE_QUOTE':
       index = state.findIndex(quote => quote.id === action.quoteId);
       quote = state[index];
@@ -28,7 +31,8 @@ export default function quotes(state = [], action) {
         ];
       }
       return state;
-    default:
+
+    default: 
       return state;
   }
 }
